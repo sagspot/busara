86cefda908f216f9f359cbf24425283a67c85ee2
Oliver Sagala <oliversagal1@gmail.com>
2021-07-11 14:18:29 +0300

    add homepage (display all forms)

--- src/components/survey/home/Forms.js
+++ src/components/survey/home/Forms.js
@@ -1,21 +1,34 @@
+import './form.scss';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../../layout/Layout';
-const FAKE_URL = 'https://fakestoreapi.com';
+import axios from 'axios';
+import { baseurl } from '../../../config';
 
 const Forms = () => {
   useEffect(() => {
-    fetchItems();
+    getForms();
   }, []);
 
   const [loading, setLoading] = useState(true);
-  const [items, setItems] = useState([]);
+  const [forms, setForms] = useState([]);
 
-  const fetchItems = async () => {
-    const response = await fetch(`${FAKE_URL}/products`);
-    const items = await response.json();
-    setItems(items);
-    setLoading(false);
+  const getForms = async () => {
+    try {
+      const token = JSON.parse(localStorage.getItem('access_token'));
+
+      const response = await axios.get(
+        `${baseurl}/api/v1/recruitment/forms/?node_type=Both`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setLoading(false);
+      setForms(response.data.forms);
+      console.log(response.data.forms);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const showLoading = () => (
@@ -24,19 +37,46 @@ const Forms = () => {
     </div>
   );
 
-  const showItems = () => (
-    <div className="">
-      {items.map((item) => (
-        <div key={item.id}>
-          <Link to={`/${item.id}`}>
-            <h3 className="mb-3">{item.title}</h3>
+  const formatDate = (string) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return new Date(string).toLocaleDateString([], options);
+  };
+
+  const showForms = () => (
+    <section className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      {forms.map((form) => (
+        <article className="col" key={form.id}>
+          <Link to={`/${form.id}`} className="card shadow-sm">
+            <div className="card-body">
+              <h2 className="fs-5 mt-4 text-center pri-brand">{form.name}</h2>
+              <p className="mt-3 text-muted">{form.description}</p>
+              <hr className="my-3" />
+              <div className="">
+                <p className="text-muted mb-2 my-fs-7">
+                  Created on <span>{formatDate(form.created)}</span>
+                </p>
+                <p className="text-muted mb-2 my-fs-7">
+                  Last modified : <span>{formatDate(form.modified)}</span>
+                </p>
+                <p className="text-muted mb-2 my-fs-7">
+                  Valid{' '}
+                  <span className="fst-italic">
+                    {formatDate(form.valid_from)}
+                  </span>{' '}
+                  to{' '}
+                  <span className="fst-italic">
+                    {formatDate(form.valid_to)}
+                  </span>
+                </p>
+              </div>
+            </div>
           </Link>
-        </div>
+        </article>
       ))}
-    </div>
+    </section>
   );
 
-  return <Layout>{loading ? showLoading() : showItems()}</Layout>;
+  return <Layout>{loading ? showLoading() : showForms()}</Layout>;
 };
 
 export default Forms;
--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
 export const baseurl = process.env.REACT_APP_API_URL;
-export const fakeapi = 'https://sagspot-shop.herokuapp.com';
